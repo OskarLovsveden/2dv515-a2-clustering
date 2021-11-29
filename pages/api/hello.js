b7792fa62ff6640b0c0effc11f412833c0bbe2d1
ol222hf <ol222hf@student.lnu.se>
2021-11-29 23:26:23 +0100

    readfile as method, data processing as own class

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,47 +1,36 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import fs from 'fs'
-import readline from 'readline'
+import fs from "fs";
+import readline from "readline";
 
-const readFile = async () => {
-  const raw_data = []
-  
-  const reader = readline.createInterface({
-    input: fs.createReadStream('data/blogdata.txt'),
-    crlfDelay: Infinity
-  })
-  
-  for await (const line of reader) {
-    raw_data.push(line.split('\t'))
-  }
-
-  const data = {
-    words: raw_data[0].length - 1,
-    blogs: [ ]
-  }
-
-  for await (const [i, row] of raw_data.entries()) {
-    if (i == 0) continue
-
-    const blog = { }
-
-    for await (const [i, key] of raw_data[0].entries()) {
-        blog[key] = row[i]
-    }
-
-    data.blogs.push(blog)
-  }
-
-  return data
-}
+import { kMeans } from "utils/clustering";
+import DataSet from "utils/DataSet";
 
 export default async function handler(req, res) {
   const {
-    body: { },
+    body: {},
     method,
-  } = req
+  } = req;
 
-  if (method === 'GET') {
-    res.status(200).json(await readFile())
+  if (method === "GET") {
+    const ds = new DataSet(await readFile());
+    await ds.process();
+
+    // const clusters = kMeans(ds);
+
+    res.status(200).json(ds.max);
   }
 }
 
+const readFile = async () => {
+  const reader = readline.createInterface({
+    input: fs.createReadStream("data/blogdata.txt"),
+    crlfDelay: Infinity,
+  });
+
+  const rawData = [];
+
+  for await (const line of reader) {
+    rawData.push(line.split("\t"));
+  }
+
+  return rawData;
+};
