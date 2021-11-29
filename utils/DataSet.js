ad0c9736d665c24b5d210bb2047a21be4ac1d677
ol222hf <ol222hf@student.lnu.se>
2021-11-29 23:27:22 +0100

    data processing as class

---
+++ utils/DataSet.js
@@ -0,0 +1,41 @@
+export default class DataSet {
+  rawData = [];
+  wordCount = 0;
+  blogs = [];
+  min = [];
+  max = [];
+
+  constructor(rawData) {
+    this.rawData = rawData;
+  }
+
+  async process() {
+    this.wordCount = this.rawData[0].length - 1;
+
+    for await (const [i, row] of this.rawData.entries()) {
+      if (i == 0) continue;
+
+      const blog = {};
+
+      for await (const [i, key] of this.rawData[0].entries()) {
+        const value = parseInt(row[i]);
+
+        blog[key] = value;
+
+        if (i == 0) {
+          continue;
+        }
+
+        if (this.min[i - 1] == null || value < this.min[i - 1]) {
+          this.min[i - 1] = value;
+        }
+
+        if (this.max[i - 1] == null || value > this.max[i - 1]) {
+          this.max[i - 1] = value;
+        }
+      }
+
+      this.blogs.push(blog);
+    }
+  }
+}
