020386957a518bf52ca865551626cf91b371cb67
OskarLovsveden <oskarlovsveden@hotmail.com>
2021-11-30 16:20:29 +0100

    refactor

--- utils/algorithms.js
+++ utils/algorithms.js
@@ -1,13 +1,17 @@
+import Blog from "models/Blog";
+import Centroid from "models/Centroid";
+
 /**
  * Pearson distance/similarity between two blogs.
  * https://coursepress.lnu.se/courses/web-intelligence/7d3df09086dc550ee1efa29239170531/3-Clustering.pdf
  *
  * @param {number} n the total number of words in all blogs.
- * @param {Blog} blogA the first blog.
- * @param {Blog} blogB the second blog.
+ * @param {Blog | Centroid} a the first blog or centroid.
+ * @param {Blog | Centroid} b the second blog or centroid.
  * @returns the similarity.
  */
-export const pearson = async (n, blogA, blogB) => {
+export const pearson = (n, a, b) => {
+
   //Init variables
   let sumA = 0,
     sumB = 0,
@@ -20,18 +24,18 @@ export const pearson = async (n, blogA, blogB) => {
 
   //Iterate over all words
   for (let i = 0; i < n; i++) {
-    cntA = blogA.wordCount(i); //word counts for each word in A
-    cntB = blogB.wordCount(i); //word counts for each word in B
+    const cntA = a.wordCount(i); //word counts for each word in A
+    const cntB = b.wordCount(i); //word counts for each word in B
     sumA += cntA; //sum of word counts for A
     sumB += cntB; //sum of word counts for B
-    sumAsq += cntA ** 2; //sum of squared word counts for A
-    sumBsq += cntB ** 2; //sum of squared word counts for B
+    sumAsq += Math.pow(cntA, 2); //sum of squared word counts for A
+    sumBsq += Math.pow(cntB, 2); //sum of squared word counts for B
     pSum += cntA * cntB; //product of word counts from A and B
   }
 
   //Calculate Pearson
-  num = pSum - (sumA * sumB) / n;
-  den = sqrt(
+  const num = pSum - (sumA * sumB / n);
+  const den = Math.sqrt(
     (sumAsq - Math.pow(sumA, 2) / n) * (sumBsq - Math.pow(sumB, 2) / n)
   );
 
